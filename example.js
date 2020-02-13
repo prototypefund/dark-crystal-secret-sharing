174e55a10425239882ce0af6c53f4ff2241760a8
ameba23 <ameba23@systemli.org>
2020-02-13 17:34:19 +0100

    update example

--- example.js
+++ example.js
@@ -7,8 +7,7 @@ console.log('Secret to share:', message)
 console.log('Creating 5 shares, 3 needed to recover')
 secrets.share(message, 5, 3).then((shares) => {
   console.log(shares.map(s => s.toString('hex')))
-  shares.pop()
-  secrets.combine(shares).then((result) => {
-    console.log('Result of recombining:', result.toString())
+  secrets.combine(shares.slice(2)).then((result) => {
+    console.log('Result of recombining 3 shares:', result.toString())
   })
 })
